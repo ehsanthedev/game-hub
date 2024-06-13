f4c024233a2a0b817338766fdfe8432224cd3218
ehsanthedev <ehsNBkhsh9499815@gmail.com>
2024-06-13 23:12:10 +0500

    learning grid , grid item and show. making responsive design with chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 
 const App = () => {
-  return (
-    <div>
-      <>
-      <div>
-      <Button colorScheme='blue'>Button</Button>
-      </div>
-      </>
-    </div>
+  return(
+    <>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area={'nav'} bg={"yellow"}>nav</GridItem>
+      <Show above="lg">
+        <GridItem area={'aside'} bg={"green"}>aside</GridItem>
+      </Show>
+      <GridItem area={'main'} bg={"red"}>main</GridItem>
+    </Grid>
+    </>
   )
 }
 
